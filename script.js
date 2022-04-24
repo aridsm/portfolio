582922718fbf82e67e7ce03ff626e70dba01db36
Ariane Morelato <aniemorelanie@gmail.com>
2022-04-24 12:47:02 -0300

    css

---
+++ script.js
@@ -0,0 +1,11 @@
+const projetos = [
+    {
+        titulo: 'Sorveteria',
+        img: 'sorveteria.gif',
+        website: 'https://aridsm.github.io/sorveteria/',
+        repositorio: 'https://github.com/aridsm/sorveteria',
+        descricao: 'Projeto de landing page de uma sorveteria fictícia feita com Html, Sass e JS.',
+        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
+        linguagens: ['HTML', 'Sass', 'JavaScript']
+    },
+]
