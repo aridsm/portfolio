04c683d737d0428ce236a3989d9e7642c47a2e50
Ariane Morelato <aniemorelanie@gmail.com>
2022-05-02 11:52:29 -0300

    att

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const projetos = [
         website: 'https://aridsm.github.io/sorveteria/',
         repositorio: 'https://github.com/aridsm/sorveteria',
         descricao: 'Projeto de landing page de uma sorveteria fictícia feita com Html, Sass e JS.',
-        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
         linguagens: ['HTML', 'Sass', 'JavaScript']
     },
     {
@@ -14,7 +13,6 @@ const projetos = [
         website: 'https://aridsm.github.io/tvsearch/',
         repositorio: 'https://github.com/aridsm/tvsearch',
         descricao: 'Projeto de um website com informações de filmes, séries e personalidades da TV feito com a API TMDB.',
-        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
         linguagens: ['React Js', 'CSS', 'JavaScript', 'HTML']
     },
     {
@@ -23,8 +21,7 @@ const projetos = [
         website: 'https://aridsm.github.io/pomodoro-app',
         repositorio: 'https://github.com/aridsm/pomodoro-app',
         descricao: 'Projeto de um cronômetro aplicado a técnica pomodoro.',
-        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
-        linguagens: ['HTML', 'Sass', 'JavaScript']
+        linguagens: ['HTML', 'CSS', 'JavaScript']
     },
     {
         titulo: 'Cafeteria',
@@ -32,7 +29,6 @@ const projetos = [
         website: 'https://aridsm.github.io/cafeteria/',
         repositorio: 'https://github.com/aridsm/cafeteria',
         descricao: 'Projeto de landing page de uma cafeteria fictícia feita com Html, Sass e JS.',
-        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
         linguagens: ['HTML', 'Sass', 'JavaScript']
     },
     {
@@ -41,64 +37,90 @@ const projetos = [
         website: 'https://aridsm.github.io/clinica-vet/',
         repositorio: 'https://github.com/aridsm/clinica-vet',
         descricao: 'Design de landing page de uma clínica veterinária fictícia. Com seções de especialidades, exames, formulário de mensagens, horário de funcionamento em tempo real etc.',
-        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
         linguagens: ['HTML', 'Sass', 'JavaScript']
     },
     {
-        titulo: 'Tela de login',
-        img: 'login.gif',
-        website: 'https://aridsm.github.io/Tela-de-login/',
-        repositorio: 'https://github.com/aridsm/Tela-de-login',
-        descricao: 'Projeto feito para aplicação de validação com Regex em campos de usuário, email e senha.',
-        maisInfos: ['A partir de 300', 'Montserrat', 'Paleta de cores'],
-        linguagens: ['HTML', 'CSS', 'JavaScript']
+        titulo: 'Canil',
+        img: 'canil.gif',
+        website: 'https://aridsm.github.io/https://aridsm.github.io/canil/',
+        repositorio: 'https://github.com/aridsm/https://aridsm.github.io/canil/',
+        descricao: 'Projeto de um canil fictício.',
+        linguagens: ['HTML', 'Sass', 'JavaScript']
     },
 ]
 
-const tituloProjeto = document.querySelector('.projeto h3'),
-linkWebsite = document.querySelector('.projeto .link-website'),
-linkRepositorio = document.querySelector('.projeto .link-repositorio'),
-descricaoProjeto = document.querySelector('.projeto .descricao-projeto'),
-imgProjeto = document.querySelector('.projeto img'),
-linguagensProjeto = document.querySelector('.projeto .linguagens ul'),
-selecaoBtnsProjetos = document.querySelector('.selecao-btn'),
-btnsPagProjetos = document.querySelectorAll('.pag-btn')
+/* Projetos */
+const
+    tituloProjeto = document.querySelector('.projeto h3'),
+    linkWebsite = document.querySelector('.projeto .link-website'),
+    linkRepositorio = document.querySelector('.projeto .link-repositorio'),
+    descricaoProjeto = document.querySelector('.projeto .descricao-projeto'),
+    imgProjeto = document.querySelector('.projeto img'),
+    linguagensProjeto = document.querySelector('.projeto .linguagens ul'),
+    selecaoBtnsProjetos = document.querySelector('.selecao-btn'),
+    btnsPagProjetos = document.querySelectorAll('.pag-btn'),
+    btnProjetoProximo = document.querySelector('.btns-nav .posterior'),
+    btnProjetoAnterior = document.querySelector('.btns-nav .anterior')
+    let indexProjeto = 0
 
-/*
--3px 0
-28px 1
-60px 2
-*/
 function choseProjectToShow(index) {
     showProject(index);
     btnsPagProjetos.forEach(btn => {
         btn.classList.remove('selecionado')
     })
     btnsPagProjetos[index].classList.add('selecionado');
-    selecaoBtnsProjetos.style.left = (index * 31) - (index * 2) + 'px';
+    selecaoBtnsProjetos.style.left=( 28 * index) - 2 + 'px';
+    indexProjeto = index
 }
 
 function showProject(index) {
     tituloProjeto.innerText = projetos[index].titulo;
     descricaoProjeto.innerText = projetos[index].descricao;
-    imgProjeto.setAttribute('src', `./img/${projetos[index].img}`) 
-    linkWebsite.setAttribute('href', projetos[index].website)
-    linkRepositorio.setAttribute('href', projetos[index].repositorio)
-    linguagensProjeto.innerHTML = ''
+    imgProjeto.setAttribute('src', `./img/${projetos[index].img}`);
+    linkWebsite.setAttribute('href', projetos[index].website);
+    linkRepositorio.setAttribute('href', projetos[index].repositorio);
+    linguagensProjeto.innerHTML = '';
     projetos[index].linguagens.map(linguagem => {
         const novaLinguagem = document.createElement('li');
         novaLinguagem.innerText = linguagem;
         linguagensProjeto.appendChild(novaLinguagem);
     })
-
 }
 
-showProject(0)
+showProject(indexProjeto)
 
+btnProjetoProximo.addEventListener('click', () => {
+    ++indexProjeto;
+    if (indexProjeto > projetos.length - 1) indexProjeto = 0;
+    choseProjectToShow(indexProjeto)
+})
+btnProjetoAnterior.addEventListener('click', () => {
+    --indexProjeto;
+    if (indexProjeto < 0) indexProjeto = projetos.length - 1;
+    choseProjectToShow(indexProjeto)
+})
 btnsPagProjetos.forEach((btn, index) => {
     btn.addEventListener('click', () => {
         choseProjectToShow(index)
     })
 })
 
-console.log(btnsPagProjetos)
+/* Sobre */
+
+const
+    btnsSobre = document.querySelectorAll('.btns-sobre button'),
+    secoesSobre = document.querySelectorAll('.container-infos > div')
+
+function mostrarSecao(index) {
+    secoesSobre.forEach(secao => secao.classList.remove('ativo'))
+    secoesSobre[index].classList.add('ativo');
+
+    btnsSobre.forEach(btn => btn.classList.remove('selecionado'))
+    btnsSobre[index].classList.add('selecionado')
+}
+
+mostrarSecao(0)
+
+btnsSobre.forEach((btn, index) => {
+    btn.addEventListener('click', () => mostrarSecao(index))
+})
